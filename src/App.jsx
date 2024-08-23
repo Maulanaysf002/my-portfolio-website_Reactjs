4a77ee8b83877e3006597500e5090019834440c6
maulanaysf002 <maulanaysf.dev@gmail.com>
2024-08-23 15:10:45 +0700

    Fix Responsive Layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,17 @@ function App() {
   useEffect(() => {
     AOS.init();
   }, []);
+
   return (
     <main className="bg-black flex flex-col justify-between">
-      <div className="flex flex-col lg:flex-row p-5 text-white gap-3 h-auto w-full">
+      <div className="flex flex-col overflow-x-hidden lg:flex-row p-5 text-white gap-3 xl:h-screen">
         <PersonalData />
-        <div className="flex flex-col gap-3 sm:w-[60%] w-full">
+        <div className="flex flex-col gap-3 lg:w-[60%]">
           <Contact />
           <Skils />
         </div>
       </div>
-      <div className="flex p-5 text-white gap-3 h-screen">
+      <div className="flex p-5 text-white gap-3 lg:h-screen bg-zinc-950">
         <Information />
       </div>
     </main>
--- src/components/PersonalData.jsx
+++ src/components/PersonalData.jsx
@@ -2,7 +2,7 @@ import { img } from '../assets/assets';
 
 const PersonalData = () => {
   return (
-    <div data-aos="fade-up" data-aos-duration="1000" className="sm:w-[40%] w-full h-full bg-neutral-900 rounded-lg p-10 cursor-pointer border-blue-400 hover:border-2 hover:ease-in hover:duration-300">
+    <div data-aos="fade-up" data-aos-duration="1000" className="lg:w-[40%] h-full bg-neutral-900 rounded-lg p-10 cursor-pointer border-blue-400 hover:border-2 hover:ease-in hover:duration-300">
       <div className="flex flex-col items-center justify-center text-center">
         <img src={img.profile_img} alt="" className="w-5/12 rounded-full" />
         <p className="mt-5">Hi, I`am</p>
